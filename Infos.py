b8b616778eb9f2ae893cf0f9f96b0dc72b8747a4
Phenryx <benedetto.abbenanti@gmail.com>
2013-03-16 18:19:28 +0100

    Update
    Organization of the plugin in two module:
    - Run.py the Core Plugin
    - Infos.py the current View Information Object.
    + Others bug fixes.

---
+++ Infos.py
@@ -0,0 +1,25 @@
+import sublime, sublime_plugin
+import os, json
+
+class Infos(object):
+	def __init__(self,view):
+		db_file = os.path.join(sublime.packages_path(), "Run", "Run.sublime-run")
+		format_file = os.path.join(sublime.packages_path(), "Run", "Run.format-name")
+		with open(db_file, "rU") as database:
+			self.database = json.load(database)
+		with open(format_file, "rU") as format:
+			self.format = json.load(format)
+		self.file_full = view.file_name()
+		self.file_path = os.path.split(view.file_name())[0]
+		self.file_name = os.path.split(view.file_name())[-1]
+		self.file_extension = self.file_name.split(".")[-1]
+		self.file_base_name = os.path.basename(self.file_path)
+		self.file_name_blank = self.file_name.replace("."+self.file_extension, "")
+		self.packages = ""
+		self.project = ""
+		self.project_path = ""
+		self.project_name = ""
+		self.project_extension = ""
+		self.project_base_name = ""
+		self.syntax = view.settings().get("syntax").split("/")[1].lower()
+		self.platform = sublime.platform()
--- Run.py
+++ Run.py
@@ -7,82 +7,46 @@ Author: Benedetto "phenryx" Abbenanti
 import sublime, sublime_plugin
 import subprocess, os, sys
 import json
+import Infos
 
 class RunCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		infos = {}
-		database = os.path.join(sublime.packages_path(), "Run", "Run.sublime-run")
-		format = os.path.join(sublime.packages_path(), "Run", "Run.format-name")
-		with open(database, "rU") as db:
-			db = json.load(db)
-		with open(format, "rU") as format:
-			format = json.load(format)
-		infos["file_full"] = self.view.file_name()
-		infos["file_path"] = os.path.split(self.view.file_name())[0]
-		infos["file_name"] = os.path.split(self.view.file_name())[-1]
-		infos["file_extension"] = infos["file_name"].split(".")[-1]
-		infos["file_base_name"] = os.path.basename(infos["file_path"])
-		infos["file_name_blank"] = infos["file_name"].replace("."+infos["file_extension"], "")
-		infos["packages"] = ""
-		infos["project"] = ""
-		infos["project_path"] = ""
-		infos["project_name"] = ""
-		infos["project_extension"] = ""
-		infos["project_base_name"] = ""
-		infos["syntax"] = self.view.settings().get("syntax").split("/")[1]
-		infos["platform"] = sublime.platform()
-		self.run_command(infos, db, format)
+		current_view = self.view
+		infos = Infos.Infos(current_view)
+		self.run_command(infos)
 
-	def run_command(self, infos, db, format):
-		syntax = infos["syntax"].lower()
-		if syntax.lower() not in db.keys():
+	def run_command(self, infos):
+		syntax = infos.syntax
+		if syntax not in infos.database.keys():
 			return None
-		self.view.run_command("echo", {"Tempus": "Irreparabile", "Fugit": "."})
-		arguments = self.arguments(syntax, infos, db, format)
+		arguments = self.arguments(infos)
 		self.view.window().run_command("exec", arguments)
 
-	def arguments(self, syntax, infos, db, format):
-		load_args = db[syntax]
+	def arguments(self, infos):
+		load_args = infos.database[infos.syntax]
 		arguments = {}
 		arguments["shell"] = True
 		for command in load_args:
 			if isinstance(load_args[command], list):
-				lst_arguments = self.lst_arguments(infos, load_args[command], format)
+				lst_arguments = self.lst_arguments(infos, load_args[command])
 				arguments[command] = lst_arguments
 				continue
-			elif load_args[command] in format: #and isinstance(load_args[command], str):
-				arguments[command] = infos[format[command]]
+			elif load_args[command] in infos.format:
+				arguments[command] = infos.format[command]
 				continue
 			elif isinstance(load_args[command], dict):
-				sub_arguments = self.arguments(syntax, infos, load_args[command], format)
+				sub_arguments = self.arguments(infos, load_args[command])
 				arguments[command] = sub_arguments
 				continue
 			arguments[command] = load_args[command]
 		return arguments
 
-	def lst_arguments(self, infos, load_args_sub, format):
+	def lst_arguments(self, infos, load_args_sub):
 		lst_arguments = []
 		for command in load_args_sub:
-			if command in format:
-				lst_arguments.append(infos[format[command]])
+			if command in infos.format:
+				command_eval = eval("infos."+"format[\""+command+"\"]")
+				lst_arguments.append(eval("infos."+command_eval))
 				continue
 			lst_arguments.append(command)
 		return lst_arguments
-
-class Infos(object):
-	def __init__(self):
-		infos["file_full"] = self.view.file_name()
-		infos["file_path"] = os.path.split(self.view.file_name())[0]
-		infos["file_name"] = os.path.split(self.view.file_name())[-1]
-		infos["file_extension"] = infos["file_name"].split(".")[-1]
-		infos["file_base_name"] = os.path.basename(infos["file_path"])
-		infos["file_name_blank"] = infos["file_name"].replace("."+infos["file_extension"], "")
-		infos["packages"] = ""
-		infos["project"] = ""
-		infos["project_path"] = ""
-		infos["project_name"] = ""
-		infos["project_extension"] = ""
-		infos["project_base_name"] = ""
-		infos["syntax"] = self.view.settings().get("syntax").split("/")[1]
-		infos["platform"] = sublime.platform()
-
