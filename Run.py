6d8bdad56bb6d978c0b62c3b4d07c8d43d9bb6c6
Phenryx <benedetto.abbenanti@gmail.com>
2013-06-30 16:23:45 +0200

    add '--' syntax for sublime-run and clean code

--- Run.py
+++ Run.py
@@ -12,16 +12,21 @@ import Infos
 class RunCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		print "--- Run - Sublime Text 2 Plugin"
+
 		current_view = self.view
 		infos = Infos.Infos(current_view)
 		self.run_command(infos)
+
 		print "With a stdin for the plugin: " + infos.stdin
 
 	def run_command(self, infos):
 		syntax = infos.syntax
+
 		if syntax not in infos.database.keys():
 			return None
+
 		arguments = self.arguments(infos)
+		print arguments
 		self.view.window().run_command("exec", arguments)
 
 	def arguments(self, infos):
@@ -33,48 +38,26 @@ class RunCommand(sublime_plugin.TextCommand):
 		arguments = {}
 		arguments["shell"] = True
 
-		for command in load_args:
-			if isinstance(load_args[command], list):
-				cmd_args = load_args[command]
+		for cmd in load_args:
+			if isinstance(load_args[cmd], list):
+				cmd_args = load_args[cmd]
+				lst_arguments = ""
 
-				#lst_arguments = "['"
-				lst_arguments = []
 				for command in cmd_args:
-					if command in infos.format:
-						#command_eval = eval("infos.format[\""+command+"\"]")
-						command_eval = infos.format[command]
-						#lst_arguments += " " + eval("infos."+command_eval)
-						#lst_arguments += " " + infos.__dict__[command_eval]
-						lst_arguments.append(infos.__dict__[command_eval])
-						#continue
-					elif command[0:2] == "--":
-						#lst_arguments += "" + command[2:]
-						lst_arguments.append(lst_arguments.pop()+command[2:])
-					else:
-						#lst_arguments += " " + command
-						lst_arguments.append(command)
 
-				#lst_arguments = self.lst_arguments(infos, load_args[command])
-				arguments[command] = eval(lst_arguments)
-				#continue
+					if command in infos.format:
+						command_eval = infos.format[command]
+						lst_arguments += " " + infos.__dict__[command_eval]
+
+					elif command[0:2] == "--":
+						lst_arguments = lst_arguments[:-1] + command[2:]
+
+					else:
+						lst_arguments += " " + command
+
+				arguments[cmd] = [lst_arguments]
+
 			else:
 				print "Error in " + infos.db_file + "\n" + infos.syntax + "'s syntax is irregular."
-			#elif load_args[command] in infos.format:
-			#	arguments[command] = infos.format[command]
-			#	continue
-			#arguments[command] = load_args[command]
 
 		return arguments
-
-"""
-	def lst_arguments(self, infos, cmd_args):
-		lst_arguments = "['"
-		for command in cmd_args:
-			if command in infos.format:
-				command_eval = eval("infos."+"format[\""+command+"\"]")
-				lst_arguments += " " + eval("infos."+command_eval)
-				continue
-			lst_arguments += " " + command
-		lst_arguments += "']"
-		return lst_arguments
-"""
